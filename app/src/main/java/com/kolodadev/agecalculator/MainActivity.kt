706212929392634bdf026112fb65926fa3dea2ba
kolod <kolod@g.com>
2021-12-14 05:02:04 +0300

    new update

--- app/src/main/java/com/kolodadev/agecalculator/MainActivity.kt
+++ app/src/main/java/com/kolodadev/agecalculator/MainActivity.kt
@@ -13,47 +13,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        editYear.filters = arrayOf(InputFilterMinMax(1,2100))
-
         btnCalculate.setOnClickListener {
             calculateAge()
         }
-    }
-
-    class InputFilterMinMax: InputFilter{
-        private var min:Int = 0
-        private var max:Int = 0
-        constructor(min:Int , max:Int){
-            this.min = min
-            this.max = max
-        }
-        constructor(min:String , max:String){
-            this.min = Integer.parseInt(min)
-            this.max = Integer.parseInt(max)
-        }
-
-        override fun filter(
-            source: CharSequence,
-            start: Int,
-            end: Int,
-            dest: Spanned?,
-            dstart: Int,
-            dend: Int): CharSequence? {
-
-            try {
-               val input = Integer.parseInt(dest.toString() +source.toString())
-               if (isInRange(min,max,input))
-                   return null
-            }
-            catch (nfe:NumberFormatException){}
-            return ""
-        }
-
-        private fun isInRange(min: Int, max: Int, input: Int): Boolean {
-            return if (max > min) input in min..max else input in max..min
-        }
-
     }
 
     private fun calculateAge() {
