7806509e6b1562826c18487b4ad45847dfe3b2dc
KolodDev <korodoafe@gmail.com>
2021-12-25 00:18:53 +0300

    update

--- app/src/main/java/com/kolodadev/agecalculator/Welcome.kt
+++ app/src/main/java/com/kolodadev/agecalculator/Welcome.kt
@@ -3,19 +3,24 @@ package com.kolodadev.agecalculator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_welcome.*
+import android.os.Handler
+import android.os.Looper
 
 class Welcome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
-        welcome.alpha = 0f
-        welcome.animate().setDuration(2000).alpha(1f).withEndAction{
+        loginToActivity()
+
+        }
+
+    private fun loginToActivity() {
+        Handler(Looper.myLooper()!!).postDelayed({
             val i = Intent(this, MainActivity::class.java)
             startActivity(i)
             overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
             finish()
-        }
+        }, 3000)
     }
 }
